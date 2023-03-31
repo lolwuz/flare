4e9871e7bf96643730af2c627bfc3cf600cfb7b5
Marten Hoekstra <martenhoekstra2@gmail.com>
2023-03-31 19:05:51 +0200

    Commit Messages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,14 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import { api } from "~/utils/api";
+import { Inter, Poppins } from "next/font/google";
 
 import "~/styles/globals.css";
+
+const inter = Poppins({
+  weight: ["400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +18,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <div className={`${inter.className} bg-neutral-900`}>
+        <Component {...pageProps} />
+      </div>
     </SessionProvider>
   );
 };
